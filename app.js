0aabddb826ed8b429e8c5f24758121428748c192
ldi569321 <dragon569321@naver.com>
2022-10-03 00:18:49 +0900

    10/03 12:19

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let count = 4;
 let clickCount = 50;
-let miss = -1;
+let miss = 0;
 
+
+let timeStop = true;
+const time = document.querySelector(".counter");
 let gamestatus = false;
 
 const start = document.querySelector(".start");
 start.addEventListener("click", () => {
     gamestatus = true;
+    time.style.display = "block";
     start.style.display = "none";
     for (i = 0; i < 50; i++) {
         doctarget[i].style.top = `${Math.floor(Math.random() * 101)}%`;
@@ -31,22 +35,69 @@ doctarget.forEach((doctarget) => {
                 nextClick();
                 missCount.innerHTML = `현재 타겟 적중률은 ${100 - miss}%입니다.`;
                 if (count == 54) {
-                    timetStop = true;
+                    timeStop = true;
+                    gamestatus = false;
+                    GameEnd();
                 }
             } else {
                 miss++;
                 missCount.innerHTML = `현재 타겟 적중률은 ${100 - miss}%입니다.`;
+                gameOver()
             }
         }
     })
 })
 box.addEventListener("click", function a() {
-    if (gamestatus == true) {
-    miss++;;
-    missCount.innerHTML = `현재 타겟 적중률은 ${100 - miss}%입니다.`;
+    if (timeStop == false) {
+        miss++;
+        missCount.innerHTML = `현재 타겟 적중률은 ${100 - miss}%입니다.`;
+        gameOver()
     }
 });
 
+
+const outPage = document.querySelector(".outPage");
+const redoBtn = document.querySelector(".redo")
+const OUT = [
+    {
+        answer: "제대로 누르고있는거죠?",
+    },
+    {
+        answer: "진짜 못하는건가요?",
+    },
+    {
+        answer: "마우스 선은 꼽고 하는건가요?",
+    },
+    {
+        answer: "제가 볼땐 고양이를 시켜서 테스트 하는것 같아요.",
+    }
+]
+
+function gameOver() {
+    if (miss >= 100) {
+        missCount.innerHTML = `현재 타겟 적중률은 0%입니다.`
+        outPage.classList.toggle('on');
+        redoBtn.classList.toggle('on');
+        timeStop = true;
+        gamestatus = false;
+
+        let random = OUT[Math.floor(Math.random() * OUT.length)];
+        outPage.innerText = random.answer;
+    }
+}
+
+redoBtn.addEventListener("click", () => {
+    window.location.reload();
+});
+
+const end = document.querySelector(".end")
+
+let timeScore = 0;
+
+function GameEnd() {
+    end.classList.toggle('on');
+    end.innerText = ((timeScore / 2) * (50 / miss)) + "점입니다."
+}
 
 function nextClick() {
     if (count < 50) {
@@ -55,24 +106,18 @@ function nextClick() {
 }
 
 let a = 0, b = 0, c = 0;
-let timetStop = false;
-
 function startBtnClick() {
-    timetStop = false;
+    timeStop = false;
     let interval = setInterval(function () {
-        if (!timetStop) {
-            min = c;
+        if (!timeStop) {
             sec = b;
             ms = a;
-
-            document.querySelector(".counter").innerHTML = `${min}:${sec}:${ms}`;
+            timeScore = `${sec}` + `.${ms}`;
+            time.innerHTML = `${sec}:${ms}`;
             a++;
             if (a > 99) {
                 b++;
                 a = 0;
-            } else if (b > 59) {
-                c++;
-                b = 0;
             }
         } else {
             clearInterval(interval);
@@ -82,3 +127,5 @@ function startBtnClick() {
 };
 
 doctarget[0].addEventListener("click", startBtnClick);
+
+
